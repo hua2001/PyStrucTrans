d19356d0bed657c5e14fd46f32b149a789682626
Sherry Chen <x.ch.msti@gmail.com>
2014-04-20 23:44:12 -0500

    update

--- pystructrans/marttrans/__init__.py
+++ pystructrans/marttrans/__init__.py
@@ -1,5 +1,3 @@
 from twins import getVariants
 from lat_opt import lat_opt
 from lat_cor import lat_cor
-from lat_cor_direct import lat_cor_direct
-from read_lat_cor import read_lat_cor 
--- pystructrans/marttrans/mat_math.py
+++ pystructrans/marttrans/mat_math.py
@@ -14,7 +14,6 @@ try:
     from scipy.linalg import eig
 except:
     from numpy.linalg import eig
-import H5_FILE
 
 def mat_dot (la, lb):
     '''
@@ -98,56 +97,8 @@ def mat_eig(l, MAX_MEM=1E9):
     
 
 def unique_rows(a):
-    
-    if a.dtype == 'int16':
-        N_MAX = H5_FILE.N_MAX/4
-    else:
-        N_MAX = H5_FILE.N_MAX/16
-        
-    if len(a) <= N_MAX:
-        if isinstance(a, h5py._hl.dataset.Dataset):
-            a = a[()]
-        b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
-        _, idx = np.unique(b, return_index=True)
-        return a[idx], idx
-    else:
-        new_len = 0
-        old_len = len(a)
-        while new_len < old_len:
-            logging.warning('%d matrices are too many, using HDF5 file' % len(a))                
-            n_total = len(a)
-            n_done = 0
-            idx = np.array([])
-            while n_done < n_total:
-                n_slice = min(N_MAX, n_total-n_done)
-                logging.info('checking uniqueness of %s to %s elements out of %d' 
-                             % (H5_FILE.num2ord(n_done+1), H5_FILE.num2ord(n_done+n_slice), len(a)))
-                a_slice = np.array(a[n_done:n_done+n_slice,:])
-                b = np.ascontiguousarray(a_slice).view(np.dtype((np.void, a_slice.dtype.itemsize * a_slice.shape[1])))
-                _, c = np.unique(b, return_index=True)
-                c = np.sort(c)
-                begin = len(idx)
-                if begin == 0:
-                    idx = H5_FILE.H5_FILE.create_dataset(H5_FILE.rand_str(6), (len(c),), maxshape=(len(a),), dtype='int')
-                    d = H5_FILE.H5_FILE.create_dataset(H5_FILE.rand_str(6),(len(c), a.shape[1]),maxshape=a.shape,dtype=a.dtype)
-                else:
-                    d.resize((len(idx)+len(c), a.shape[1]))
-                    idx.resize((len(idx)+len(c),))
-                idx[begin:] = c + n_done
-                d[begin:] = a_slice[c]
-                
-                del c
-                del a_slice
-                del b
-                
-                n_done = n_done+n_slice
-                       
-            # update new and old length
-            old_len = len(a)
-            a = d 
-            new_len = len(a)   
-            if new_len <= N_MAX:
-                return unique_rows(a[()])                 
-        return a, idx
+    b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
+    _, idx = np.unique(b, return_index=True)
+    return a[idx], idx
     
     
