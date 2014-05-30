619ef402a42f365f0a95a6e83d5804bc3906c75b
Sherry Chen <x.ch.msti@gmail.com>
2014-05-30 17:57:50 -0500

    update

--- pystructrans/crystallography/orientation_relationship.py
+++ pystructrans/crystallography/orientation_relationship.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.linalg import inv, det, eig, norm
-from math import sqrt
+from math import sqrt, cos, sin
 
 
 def direc_trans(L_cor, n_list):
@@ -36,6 +36,48 @@ def plane_trans(L_cor, n_list):
         return new_n_list[0]
     else:
         return np.array(new_n_list)
+    
+def rad(t):
+    return t*np.pi/180
+def rZ(t):
+    return np.array([[cos(t), -sin(t), 0], 
+                     [sin(t), cos(t), 0],
+                     [0, 0, 1]])
+def rX(t):
+    return np.array([[1, 0, 0], 
+                     [0, cos(t), -sin(t)],
+                     [0, sin(t), cos(t)]])
+def rY(t):
+    return np.array([[cos(t), 0, sin(t)],
+                     [0, 1, 0], 
+                     [-sin(t), 0, cos(t)]])
+
+def euler(phi_1, Psi, phi_2):
+    r'''
+    calculate rotation matrix with
+    given three Euler angles by
+    order Z-X-Z 
+    '''
+    phi_1 = rad(phi_1)
+    Psi = rad(Psi)
+    phi_2 = rad(phi_2)
+    return np.dot(rZ(phi_2), rY(Psi).dot(rX(phi_1)))
+def Rot(t, z):
+    r'''
+    calculate the rotation matrix about axis z
+    '''
+    z = np.array(z)
+    z = z/norm(z)
+    t = rad(t)
+    if z[0]**2+z[1]**2<1e-6:
+        z2 = np.array([0,1,0])
+        z1 = np.array([1,0,0])
+    else:
+        z2 = np.array([-z[1], z[0], 0])
+        z2 = z2/norm(z2)
+        z1 = np.cross(z, z2)
+    return cos(t)*(np.outer(z1, z1)+np.outer(z2,z2))+sin(t)*(np.outer(z2,z1)-np.outer(z1,z2))+np.outer(z, z)
+    
 
 
 
