dc1bc33b477f997a8381c177cd9be4bd986e57bd
Sherry Chen <x.ch.msti@gmail.com>
2014-06-26 14:32:34 -0500

    ok

--- pystructrans/marttrans/__init__.py
+++ pystructrans/marttrans/__init__.py
@@ -1,4 +1,4 @@
 from lat_cor import lat_cor
 from martensite import Martensite
 from Twin import TwinSystem, TwinSolver
-from Compatibility import AM_Solver
+from Compatibility import AM_Solver, isCompatible
